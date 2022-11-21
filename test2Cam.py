85e426ad1dd8c1c65b2c2cb2b5c6b8ca03f5806e
derp000 <84947701+derp000@users.noreply.github.com>
2022-11-21 15:20:40 -0500

    test commit

--- test2Cam.py
+++ test2Cam.py
@@ -42,8 +42,8 @@ def main():
         #
         # Insert your image processing logic here!
         #
-        # hsv_img = cv2.cvtColor(input_img, cv2.COLOR_BGR2HSV)
-        # binary_img = cv2.inRange(hsv_img, (min_hue, min_sat, min_val), (max_hue, max_sat, max_val))
+        hsv_img = cv2.cvtColor(input_img, cv2.COLOR_BGR2HSV)
+        binary_img = cv2.inRange(hsv_img, (min_hue, min_sat, min_val), (max_hue, max_sat, max_val))
 
         processing_time = time.time() - start_time
         fps = 1 / processing_time
