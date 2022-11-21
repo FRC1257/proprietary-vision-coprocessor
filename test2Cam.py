1ceec98f7b977326ec31afdf745a223bae5b1189
derp000 <84947701+derp000@users.noreply.github.com>
2022-11-21 15:33:34 -0500

    Make output_img be the bin img

--- test2Cam.py
+++ test2Cam.py
@@ -37,7 +37,7 @@ def main():
         # Tell the CvSink to grab a frame from the camera and put it
         # in the source image.  If there is an error notify the output.
         mytime, img = cvSink.grabFrame(img)
-        output_img = np.copy(img)
+        input_img = np.copy(img)
 
         cv2.putText(img, "hello", (50, 50), cv2.FONT_HERSHEY_SIMPLEX,1, 2, cv2.LINE_AA)
         
@@ -50,8 +50,11 @@ def main():
         #
         # Insert your image processing logic here!
         #
-        hsv_img = cv2.cvtColor(output_img, cv2.COLOR_BGR2HSV)
-        binary_img = cv2.inRange(hsv_img, (MIN_HUE, MIN_SAT, MIN_VAL), (MAX_HUE, MAX_SAT, MAX_SAT))
+        hsv_img = cv2.cvtColor(input_img, cv2.COLOR_BGR2HSV)
+        
+        # binary_img = cv2.inRange(hsv_img, (MIN_HUE, MIN_SAT, MIN_VAL), (MAX_HUE, MAX_SAT, MAX_SAT))
+        # just for testing, the binary_img will be outputted
+        output_img = cv2.inRange(hsv_img, (MIN_HUE, MIN_SAT, MIN_VAL), (MAX_HUE, MAX_SAT, MAX_SAT))
 
         processing_time = time.time() - start_time
         fps = 1 / processing_time
